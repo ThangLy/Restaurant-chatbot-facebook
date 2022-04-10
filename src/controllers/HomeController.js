06dafd3432137cd9b36f6341baf7fb7157dc7b89
Thang Ly <lyducthang1999@gmail.com>
2022-04-10 15:54:54 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import request from "request";
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
 //process.env.NAME_VARIABLES
 let getHomePage = (req, res) => {
   return res.render('homepage.ejs')
@@ -137,8 +139,39 @@ function callSendAPI(sender_psid, response) {
   });
 }
 
+let setupProfile = async (req, res) => {
+  //Call profile facebook api
+  // Construct the message body
+  let request_body = {
+
+    "get_started": { "payload": "GET_STARTED" },
+    "whitelisted_domains": ["https://chatbot-thangly.herokuapp.com/"]
+  }
+
+  //template string
+
+  // Send the HTTP request to the Messenger Platform
+  await request({
+    "uri": `https://graph.facebook.com/v13.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    console.log(body)
+    if (!err) {
+      console.log('Setup user profile succeeds!')
+    } else {
+      console.error("Unable to setup user profile:" + err);
+    }
+  });
+
+  return res.send('Setup user profile succeeds!');
+
+}
+
 module.exports = {
   getHomePage: getHomePage,
   postWebhook: postWebhook,
   getWebhook: getWebhook,
+  setupProfile: setupProfile,
 }
