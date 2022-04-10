6cd133ccee72453e7ac30ba3cc71f299cfc2321f
Thang Ly <lyducthang1999@gmail.com>
2022-04-10 17:22:25 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -135,7 +135,7 @@ async function handlePostback(sender_psid, received_postback) {
       response = { "text": "Oops, try sending another image." }
       break;
     case 'GET_STARTED':
-      await chatbotService.handGetStarted();
+      await chatbotService.handGetStarted(sender_psid);
       break;
     default:
       // code block
