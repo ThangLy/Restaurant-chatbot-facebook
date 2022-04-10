8201fa6da928151411b49d0851552f4b8d328712
Thang Ly <lyducthang1999@gmail.com>
2022-04-10 17:49:16 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import request from "request";
 require('dotenv').config();
 
@@ -25,10 +26,32 @@ let callSendAPI = (sender_psid, response) => {
         }
     });
 }
+
+let getUserName = async (sender_psid) => {
+    let username = '';
+    // Send the HTTP request to the Messenger Platform
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
+        "method": "GET",
+    }, (err, res, body) => {
+        console.log(body)
+        if (!err) {
+            response = JSON.parse(res);
+            username = `${response.first_name} ${response.last_name}`;
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+
+    return username;
+}
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
-            let response = { "text": "OK, Xin chào mừng bạn ABC đến với 1 con bot sắp thông minh." }
+            let username = await getUserName(sender_psid);
+            let response = { "text": `OKay, Xin chào mừng bạn ${username} đến với 1 con bot sắp thông minh.` }
             await callSendAPI(sender_psid, response);
             resolve('done');
         } catch (e) {
